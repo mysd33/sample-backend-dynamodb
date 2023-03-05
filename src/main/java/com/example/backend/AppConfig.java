0bab9bdff20f18f1fa9409f34e179b54698ad2af
mysd33 <mysd.33@gmail.com>
2023-03-05 21:58:05 +0900

    設定クラスのリファクタリング

--- src/main/java/com/example/backend/AppConfig.java
+++ src/main/java/com/example/backend/AppConfig.java
@@ -20,7 +20,7 @@ import io.swagger.v3.oas.models.info.Info;
  *
  */
 @Configuration
-public class WebConfig {
+public class AppConfig {
 
     /**
      * エラーレスポンス作成クラス
--- src/main/java/com/example/backend/InfraConfig.java
+++ src/main/java/com/example/backend/InfraConfig.java
@@ -1,9 +1,14 @@
 package com.example.backend;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.example.backend.domain.repository.TodoRepository;
-import com.example.backend.infra.repository.TodoRepositoryStub;
+import com.example.fw.common.dynamodb.DynamoDBTableInitializer;
+import com.example.fw.common.dynamodb.config.DynamoDBConfigPackage;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 /**
  * 
@@ -11,12 +16,20 @@ import com.example.backend.infra.repository.TodoRepositoryStub;
  *
  */
 @Configuration
+@ComponentScan(basePackageClasses = { DynamoDBConfigPackage.class })
 public class InfraConfig {
 
+
+    @Bean
+    public DynamoDBTableInitializer dynamoDBTableInitializer(DynamoDbClient dynamoDbClient,
+            DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        return new SampleBackendDynamoDBTableInitializer(dynamoDbClient, dynamoDbEnhancedClient);
+    }
+    
     // DBアクセスしない場合のスタブ
     // @Bean
-    public TodoRepository todoRepositoryStub() {
-        return new TodoRepositoryStub();
-    }
+//    public TodoRepository todoRepositoryStub() {
+//        return new TodoRepositoryStub();
+//    }
 
 }
