cc00e788c37ae999a361779149c0995ebe35f4f1
mysd33 <mysd.33@gmail.com>
2023-03-21 12:44:00 +0900

    X-Rayの設定クラスのフレームワーク化

--- src/main/java/com/example/backend/XRayConfig.java
+++ src/main/java/com/example/backend/XRayConfig.java
@@ -1,52 +1,16 @@
 package com.example.backend;
 
-import javax.servlet.Filter;
-
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Import;
 
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.AWSXRayRecorderBuilder;
-import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import com.amazonaws.xray.plugins.EC2Plugin;
-import com.amazonaws.xray.plugins.ECSPlugin;
-import com.amazonaws.xray.plugins.EKSPlugin;
-import com.amazonaws.xray.spring.aop.BaseAbstractXRayInterceptor;
+import com.example.fw.web.servlet.config.XRayServletConfig;
 
 /**
  * X-Rayの設定クラス
  *
  */
-@Profile("xray")
-@Aspect
+// X-Ray機能の追加
+@Import({ XRayServletConfig.class })
 @Configuration
-public class XRayConfig extends BaseAbstractXRayInterceptor {
-    static {
-        // サービスプラグインの設定
-        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EKSPlugin())
-                .withPlugin(new ECSPlugin()).withPlugin(new EC2Plugin());
-        // TODO: サンプリングルール
-        // URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-        // builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
-
-        AWSXRay.setGlobalRecorder(builder.build());
-    }
-
-    @Override
-    @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled) " + " && execution(* com.example..*.*(..))")
-    protected void xrayEnabledClasses() {
-    }
-
-    /**
-     * AWS X-Rayによる分散トレーシングの設定
-     * 
-     */
-    @Bean
-    public Filter tracingFilter() {
-        return new AWSXRayServletFilter("sample-backend-dynamodb");
-    }
-
+public class XRayConfig {
 }
