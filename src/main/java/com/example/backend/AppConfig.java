70d122930215d426017f0badce6448a482d7416b
mysd33 <mysd.33@gmail.com>
2024-02-12 11:53:08 +0900

    SpringBoot3.2バージョンアップとAPIエラーレスポンスの対応

--- src/main/java/com/example/backend/AppConfig.java
+++ src/main/java/com/example/backend/AppConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.example.backend.app.api.common.advice.ErrorResponseCreator;
+import com.example.backend.app.api.common.advice.DefaultErrorResponseCreator;
 import com.example.backend.domain.message.MessageIds;
-
+import com.example.fw.web.advice.ErrorResponseCreator;
 import com.example.fw.web.aspect.LogAspect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -27,7 +27,7 @@ public class AppConfig {
      */
     @Bean
     public ErrorResponseCreator errorResponseCreator(MessageSource messageSource) {
-        return new ErrorResponseCreator(messageSource, MessageIds.W_EX_5001, MessageIds.E_EX_9001);
+        return new DefaultErrorResponseCreator(messageSource, MessageIds.W_EX_5001, MessageIds.E_EX_9001);
     }
 
     /**
@@ -40,7 +40,7 @@ public class AppConfig {
 
     /**
      * Springdoc-openapiでスネークケースの設定が反映されるようにするための回避策
-     */
+     */    
     @Bean
     public ModelResolver modelResolver(ObjectMapper objectMapper) {
         return new ModelResolver(objectMapper);
@@ -53,4 +53,5 @@ public class AppConfig {
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI().info(new Info().title("Todo APIドキュメント").description("Todo管理のためのAPIです。").version("v1.0"));
     }
+
 }
