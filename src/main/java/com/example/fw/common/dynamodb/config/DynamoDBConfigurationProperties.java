a5f134895d11ec4c5073825215caa8a22f347558
mysd33 <mysd.33@gmail.com>
2025-10-07 23:43:45 +0900

    Spring Bootプロパティ名の定数定義化

--- src/main/java/com/example/fw/common/dynamodb/config/DynamoDBConfigurationProperties.java
+++ src/main/java/com/example/fw/common/dynamodb/config/DynamoDBConfigurationProperties.java
@@ -2,6 +2,8 @@ package com.example.fw.common.dynamodb.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import com.example.fw.common.constants.FrameworkConstants;
+
 import lombok.Data;
 
 /**
@@ -10,14 +12,18 @@ import lombok.Data;
  *
  */
 @Data
-@ConfigurationProperties(prefix = "aws.dynamodb")
+@ConfigurationProperties(prefix = DynamoDBConfigurationProperties.PROPERTY)
 public class DynamoDBConfigurationProperties {
+    // TODO: プロパティ名の見直しを予定
+    // DynamoDBの設定を保持するプロパティのプレフィックス
+    static final String PROPERTY = FrameworkConstants.PROPERTY_BASE_NAME + "aws.dynamodb";
+    // リージョン（デフォルト: ap-northeast-1）
     private String region = "ap-northeast-1";
+    // ローカルDynamoDBの設定
     private DynamoDBLocalProperties dynamodblocal;
-    
 
     @Data
     public static class DynamoDBLocalProperties {
         private int port = 8000;
-    }    
+    }
 }
