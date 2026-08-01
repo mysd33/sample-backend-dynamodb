427ad703437edf911f7c76619a6e06b6e9f09103
mysd33 <mysd.33@gmail.com>
2026-08-01 13:10:42 +0900

    本番環境のみOAuth ResourceServerが動くように修正

--- src/main/java/com/example/backend/SecurityConfig.java
+++ src/main/java/com/example/backend/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.backend;
 import static org.springframework.boot.security.autoconfigure.web.servlet.PathRequest.toStaticResources;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.security.autoconfigure.actuate.web.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -85,6 +86,7 @@ public class SecurityConfig {
     /// Spring SecurityによるOAuth2.0でのAPI認可の設定(v2 api)
     @Bean
     @Order(3)
+    @ConditionalOnProperty(name = "example.oidc.enabled", havingValue = "true", matchIfMissing = false)
     SecurityFilterChain securityFilterChainForV2Api(HttpSecurity http) {
         // v2のAPIは、OAuth2.0による認可設定を基本とする
         http.securityMatcher("/api/v2/**")
--- src/test/java/com/example/backend/SampleBackendDynamodbApplicationTests.java
+++ src/test/java/com/example/backend/SampleBackendDynamodbApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles({"dev", "log_default"})
 @SpringBootTest
 class SampleBackendDynamodbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
